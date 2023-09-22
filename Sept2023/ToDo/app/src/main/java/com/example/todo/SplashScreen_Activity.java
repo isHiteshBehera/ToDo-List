46edfa3590e9437d0b1c76420ef1c1c96da4936e
Hitesh Behera <bhiteshme74@gmail.com>
2023-09-22 17:53:31 +0530

    New: Splash Screen - SplashScreen_Activity.java - activity_splash_screen.xml
    Update:
    - AndroidManifest.xml

---
+++ Sept2023/ToDo/app/src/main/java/com/example/todo/SplashScreen_Activity.java
@@ -0,0 +1,14 @@
+package com.example.todo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashScreen_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
