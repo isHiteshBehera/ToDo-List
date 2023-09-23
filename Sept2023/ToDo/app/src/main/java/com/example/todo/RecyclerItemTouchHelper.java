585dbc6530bec3cfa34771237f5b39fb61cdfbf1
Hitesh Behera <bhiteshme74@gmail.com>
2023-09-23 23:36:02 +0530

    framework complete

---
+++ Sept2023/ToDo/app/src/main/java/com/example/todo/RecyclerItemTouchHelper.java
@@ -0,0 +1,122 @@
+package com.example.todo;
+
+import static com.google.android.material.color.utilities.MaterialDynamicColors.background;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.todo.Adapter.ToDoAdapter;
+
+public class RecyclerItemTouchHelper extends ItemTouchHelper.SimpleCallback {
+
+    private ToDoAdapter adapter;
+
+    public RecyclerItemTouchHelper(ToDoAdapter adapter) {
+        super(0, ItemTouchHelper.LEFT || ItemTouchHelper.RIGHT);
+        this.adapter = adapter;
+    }
+
+    @Override
+    public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+        return false;
+    }
+
+    @Override
+    public void onSwipe(final RecyclerView.ViewHolder viewHolder, int direction) {
+        final int position = viewHolder.getAdapterposition();
+        if (direction == ItemTouchHelper.LEFT) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(adapter.getContext());
+            builder.setTitle("Delete Task");
+            builder.setMessage("Are you sure to delete this task ?");
+            builder.setPositiveButton("Confirm",
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            adapter.deleteitem(position);
+                        }
+                    });
+            builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    adapter.notifyItemChanged(viewHolder.getAdapterPosition());
+                }
+            });
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        } else {
+            adapter.editItem(position);
+        }
+    }
+
+    @Override
+    public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder,
+                            float dX, float dY, int actionState, boolean isCurrentlyActive) {
+        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+
+        Drawable icon;
+        ColorDrawable background;
+
+        View itemView = viewHolder.itemView;
+        int backgroundCornerOffset = 20;
+
+        if (dX > 0) {
+            icon = ContextCompat.getDrawable(adapter.getContext(), R.drawable.edit_task_icon);
+            background = new ColorDrawable(ContextCompat.getColor(adapter.getContext()R.color.colorPrimary));
+        } else {
+            icon = ContextCompat.getDrawable(adapter.getContext(), R.drawable.delete_task_icon);
+            background = new ColorDrawable(Color.RED);
+        }
+
+        int iconMargin = (itemView.getHeight() - icon.getIntrinsicHeight()) / 2;
+        int iconTop = itemView.getTop() + (itemView.getHeight() - icon.getIntrinsicHeight()) / 2;
+        int iconBottom = iconTop + icon.getIntrinsicHeight();
+
+        if (dX > 0) {
+            int iconLeft = itemView.getLeft() + iconMargin;
+            int iconRight = itemView.getRight() + iconMargin + icon.getIntrinsicWidth();
+            icon.setBounds(iconleft, iconTop, iconRight, iconBottom);
+
+            background.setBounds(itemView.getLeft(), itemView.getTop(),
+                    itemView.getLeft() + ((int) dX) + backgroundCornerOffset, itemView.getBottom());
+        } else if (dX < 0) {
+            int iconLeft = itemView.getRight() - iconMargin - icon.getIntrinsicWidth();
+            int iconRight = itemView.getRight() - iconMargin;
+            icon.setBounds(iconleft, iconTop, iconRight, iconBottom);
+
+            background.setBounds(itemView.getRight() + ((int) dX) - backgroundCornerOffset, itemView.getTop(),
+                    itemView.getRight(), itemView.getBottom());
+
+        } else {
+            background.setBounds(0, 0, 0, 0);
+        }
+
+
+    }
+background.draw(c);
+icon.draw(c);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
