5a5055645f792dbec39a2e8432e3b70b89910ba6
Hitesh Behera <bhiteshme74@gmail.com>
2023-09-23 22:42:58 +0530

    declared FloatingActionButton in MainActivity.java

--- Sept2023/ToDo/app/src/main/java/com/example/todo/MainActivity.java
+++ Sept2023/ToDo/app/src/main/java/com/example/todo/MainActivity.java
@@ -6,10 +6,12 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.todo.Adapter.ToDoAdapter;
 import com.example.todo.Model.ToDoModel;
 import com.example.todo.Utils.DatabaseHandler;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -19,6 +21,7 @@ public class MainActivity extends AppCompatActivity implements DialogCloseListen
 
     private RecyclerView taskRecyclerView;
     private ToDoAdapter taskAdapter;
+    private FloatingActionButton fab;
 
     private List<ToDoModel> taskList;
     private DatabaseHandler db;
@@ -36,12 +39,21 @@ public class MainActivity extends AppCompatActivity implements DialogCloseListen
 
         RecyclerView tasksRecyclerView = findViewById(R.id.taskRecyclerView);
         tasksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        ToDoAdapter tasksAdapter = new ToDoAdapter(this);
+        ToDoAdapter tasksAdapter = new ToDoAdapter(db, this);
         tasksRecyclerView.setAdapter(tasksAdapter);
+
+        fab = findViewById(R.id.fab_add_btn);
 
         taskList = db.getAllTasks();
         Collections.reverse(taskList);
         tasksAdapter.setTasks((taskList));
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AddNewTask.newInstance().show(getSupportFragmentManager(), AddNewTask.TAG);
+            }
+        });
     }
 
     @Override
