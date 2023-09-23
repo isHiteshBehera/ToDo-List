5cf8aa4fa7bfc9fdf30666af07fcdde7266dc256
Hitesh Behera <bhiteshme74@gmail.com>
2023-09-23 12:16:36 +0530

    finish: MainActivity.java - RecyclerView - ToDoAdapter - List<ToDoModel> - protected void onCreate()     > taskList = new Arraylist<>();     > taskRecyclerView     > tasksAdapter
        > ToDoModel
        > taskList.add() [demo tasks]

--- Sept2023/ToDo/app/src/main/java/com/example/todo/MainActivity.java
+++ Sept2023/ToDo/app/src/main/java/com/example/todo/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.todo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.todo.Adapter.ToDoAdapter;
+import com.example.todo.Model.ToDoModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView tasksRecyclerView;
+    private ToDoAdapter tasksAdapter;
+    private List<ToDoModel> taskList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide(); // hide topmost navigation bar
+
+        taskList = new ArrayList<>();
+
+        tasksRecyclerView = findViewById(R.id.taskRecyclerView);
+        tasksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        tasksAdapter = new ToDoAdapter(this);
+        tasksRecyclerView.setAdapter(tasksAdapter);
+
+        ToDoModel task = new ToDoModel();
+        task.setTask("This is a test task.");
+        task.setStatus(0);
+        task.setId(1);
+
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+
+        tasksAdapter.setTasks(taskList);
+
+
+
+
     }
 }
