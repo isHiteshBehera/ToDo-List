08beb0833373dbb9b6449573c82b79e21befdf9a
Hitesh Behera <bhiteshme74@gmail.com>
2023-09-23 10:43:14 +0530

    New: SplashScreen_Activity.java - Intent - Handler     > start SplashScreen_Activity     > finish SplashScreen_Activity     > delay 1000ms

--- Sept2023/ToDo/app/src/main/java/com/example/todo/SplashScreen_Activity.java
+++ Sept2023/ToDo/app/src/main/java/com/example/todo/SplashScreen_Activity.java
@@ -2,13 +2,27 @@ package com.example.todo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreen_Activity extends AppCompatActivity {
+
+    private Object delayMillis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+
+        final Intent i = new Intent(SplashScreen_Activity.this, MainActivity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(i);
+                finish();
+            }
+        }, 1000);
     }
 }
